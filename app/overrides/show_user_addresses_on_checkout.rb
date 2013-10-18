b0135d57ddb807734ba4553fb0d39760dcd74dd6
Calvin Yu <calvin@scoutmob.com>
2013-10-18 15:10:40 -0400

    Modify override to support deface 1.0

--- app/overrides/show_user_addresses_on_checkout.rb
+++ app/overrides/show_user_addresses_on_checkout.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   :virtual_path => "spree/checkout/_address",
   :name => "show_user_addresses_on_bill_address",
-  :insert_before => %q{[data-hook=billing_fieldset_wrapper] code[erb-loud]:contains("render :partial => 'spree/address/form'")},
+  :insert_before => %q{[data-hook=billing_fieldset_wrapper] erb[loud]:contains("render :partial => 'spree/address/form'")},
   :text => "<%= render :partial => 'spree/checkout/user_addresses', :locals => {:address_type => 'billing', :form => form, :address_name => 'bill_address'} %>",
   :disabled => false
 )
@@ -9,7 +9,7 @@ Deface::Override.new(
 Deface::Override.new(
   :virtual_path => "spree/checkout/_address",
   :name => "show_user_addresses_on_ship_address",
-  :insert_before => %q{[data-hook=shipping_fieldset_wrapper] code[erb-loud]:contains("render :partial => 'spree/address/form'")},
+  :insert_before => %q{[data-hook=shipping_fieldset_wrapper] erb[loud]:contains("render :partial => 'spree/address/form'")},
   :text => "<%= render :partial => 'spree/checkout/user_addresses', :locals => {:address_type => 'shipping', :form => form, :address_name => 'ship_address'} %>",
   :disabled => false
 )
